55a626e3be116134d77c8afd00ca03c0e1ef5cfa
russellchao <russchaoman@gmail.com>
2025-10-25 21:37:22 -0400

    Created a main.py file with test cases that will act as a data pipeline.

--- pbp_situation_model.py
+++ pbp_situation_model.py
@@ -7,7 +7,27 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 
 
-def train_model(X, y):
+def train_pbp_model():
+    # Open both 2024 Play-by-Play CSV files and combine them
+    pbp_files = [pd.read_csv("Data/pbp_2024_0.csv"), pd.read_csv("Data/pbp_2024_1.csv")]
+    df = pd.concat(pbp_files, ignore_index=True)
+    print(df.columns.to_list())
+    print(df.head())
+    
+    # Filter columns that only contain "run" or "pass" for play_type
+    df_filtered = df[df['play_type'].isin(['run', 'pass'])]
+    print(f"Number of rows after filtering for run/pass plays: {df_filtered.shape[0]}")
+
+    # (game situation) x variables using categories 1-3 for now
+    X = df_filtered[['down', 'ydstogo', 'yardline_100', 'goal_to_go', 'quarter_seconds_remaining',
+            'half_seconds_remaining', 'game_seconds_remaining', 'score_differential', 'wp',
+            'ep', 'posteam_timeouts_remaining', 'defteam_timeouts_remaining', 'posteam', 'defteam']]
+    print(X.head(10))
+
+    # y variables
+    y = df_filtered['play_type']
+    print(y.head(10))
+
     # Split the data between X and y
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -97,54 +117,7 @@ def predict_play(situation, trained_model, feature_columns):
     print("======================================")
     print()
     
-    return prediction[0], prediction_proba[0]  # Return prediction and confidence
+    # Return prediction and confidence
+    return prediction[0], prediction_proba[0]  
 
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    ''' For testing only. Will eventually add a function with the same functionality below that will be called from an endpoint ''' 
-
-    # Open both 2024 Play-by-Play CSV files and combine them
-    pbp_files = [pd.read_csv("Data/pbp_2024_0.csv"), pd.read_csv("Data/pbp_2024_1.csv")]
-    df = pd.concat(pbp_files, ignore_index=True)
-    print(df.columns.to_list())
-    print(df.head())
-    
-    # Filter columns that only contain "run" or "pass" for play_type
-    df_filtered = df[df['play_type'].isin(['run', 'pass'])]
-    print(f"Number of rows after filtering for run/pass plays: {df_filtered.shape[0]}")
-
-    # (game situation) x variables using categories 1-3 for now
-    X = df_filtered[['down', 'ydstogo', 'yardline_100', 'goal_to_go', 'quarter_seconds_remaining',
-            'half_seconds_remaining', 'game_seconds_remaining', 'score_differential', 'wp',
-            'ep', 'posteam_timeouts_remaining', 'defteam_timeouts_remaining', 'posteam', 'defteam']]
-    print(X.head(10))
-
-    # y variables
-    y = df_filtered['play_type']
-    print(y.head(10))
-
-    trained_model, feature_columns = train_model(X, y)
-    print(feature_columns)
-    print()
-
-    # Test situations to predict play type - [down, ydstogo, yardline_100, goal_to_go, quarter_seconds_remaining, half_seconds_remaining, game_seconds_remaining, score_differential, wp, ep, posteam_timeouts_remaining, defteam_timeouts_remaining, posteam, defteam]
-    test_case_1 = [2, 5, 30, 0, 720, 720, 2520, 0, 0.52, 1.8, 3, 3, 'KC', 'BUF'] # 2nd & 5 from opponent's 30-yard line, Q2-12:00, tied game, balanced situation
-    test_case_2 = [3, 8, 50, 0, 180, 1080, 1080, -3, 0.42, 0.8, 2, 3, 'GB', 'DAL'] # 3rd & 8 from midfield, Q3-3:00, down by 3, passing situation
-    test_case_3 = [1, 10, 75, 0, 480, 1380, 3180, 7, 0.62, -0.4, 3, 3, 'SF', 'SEA'] # 1st & 10 from own 25-yard line, Q1-8:00, ahead by 7, balanced situation
-    test_case_4 = [1, 8, 8, 1, 95, 95, 95, -4, 0.15, 4.2, 1, 2, 'NE', 'NYG'] # 1st & Goal from 8-yard line, Q4-1:35, down by 4, red zone situation  
-    test_case_5 = [4, 2, 35, 0, 45, 45, 45, -6, 0.05, 1.2, 0, 1, 'TB', 'DET'] # 4th & 2 from opponent's 35, Q4-0:45, down by 6, desperation situation
-    test_case_6 = [3, 1, 60, 0, 600, 1500, 3300, 0, 0.50, 0.5, 2, 2, 'MIA', 'NYJ'] # 3rd & 1 from own 40, Q1-10:00, tied game, short yardage situation
-    test_case_7 = [1, 1, 1, 1, 600, 600, 2400, 0, 0.50, 0.5, 2, 2, 'PHI', 'LAR'] # 1st & Goal from the 1, Q3-10:00, Tush Push Situation for the Eagles 
-    test_case_8 = [3, 15, 80, 0, 900, 900, 900, -10, 0.30, -1.5, 1, 2, 'CIN', 'PIT'] # 3rd and long from own 20, Q4-15:00, down by 10, passing situation
-
-    all_test_cases = [test_case_1, test_case_2, test_case_3, test_case_4, test_case_5, test_case_6, test_case_7, test_case_8]
-
-    for test_case in all_test_cases:
-        predict_play(test_case, trained_model, feature_columns)
     
